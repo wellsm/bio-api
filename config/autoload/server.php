3d24773afd39e261b389c09e6697ecb30edfa86e
Well <wellington.martins@uello.com.br>
2024-02-11 20:14:47 -0300

    fix: cast port to int

--- config/autoload/server.php
+++ config/autoload/server.php
@@ -17,7 +17,7 @@ return [
             'name'      => 'http',
             'type'      => Server::SERVER_HTTP,
             'host'      => '0.0.0.0',
-            'port'      => env('PORT', 9501),
+            'port'      => (int) env('PORT', 9501),
             'sock_type' => SWOOLE_SOCK_TCP,
             'callbacks' => [
                 Event::ON_REQUEST => [\Hyperf\HttpServer\Server::class, 'onRequest'],
